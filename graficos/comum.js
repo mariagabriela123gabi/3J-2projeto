0b2c7e3c744a0f53aeaf255d74001b52ee402a57
mariagabriela123gabi <110062972+mariagabriela123gabi@users.noreply.github.com>
2024-10-24 08:04:00 -0300

    24/10

--- graficos/comum.js
+++ graficos/comum.js
@@ -1,7 +1,13 @@
 const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel)
 }
+const tickfont = {
+    color: getCSS('--cor-primaria'),
+    size: 16,
+    family: getCSS('--fonte')
 
-export{
-    getCSS
 }
+
+export{getCSS,tickfont}
+   
+
